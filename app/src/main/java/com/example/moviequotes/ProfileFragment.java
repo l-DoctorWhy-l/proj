db245c367724d67ffc86bfb4a485b9c6ce39678a
Egor K <kvartalovfade@gmail.com>
2023-03-22 18:04:54 +0300

    edit settings start

--- app/src/main/java/com/example/moviequotes/ProfileFragment.java
+++ app/src/main/java/com/example/moviequotes/ProfileFragment.java
@@ -8,17 +8,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.moviequotes.databinding.FragmentProfileBinding;
+
 
 public class ProfileFragment extends Fragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
+    FragmentProfileBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        binding.editSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.editSettingsButton.setVisibility(View.GONE);
+                binding.saveSettingsButton.setVisibility(View.VISIBLE);
+                binding.profileUsername.setVisibility(View.GONE);
+                binding.editProfileUsername.setVisibility(View.VISIBLE);
+                binding.profileSexLayout.setVisibility(View.GONE);
+                binding.editProfileSex.setVisibility(View.VISIBLE);
+            }
+        });
+
+        binding.saveSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.editSettingsButton.setVisibility(View.VISIBLE);
+                binding.saveSettingsButton.setVisibility(View.GONE);
+                binding.profileUsername.setVisibility(View.VISIBLE);
+                binding.editProfileUsername.setVisibility(View.GONE);
+                binding.profileSexLayout.setVisibility(View.VISIBLE);
+                binding.editProfileSex.setVisibility(View.GONE);
+
+
+                // FUNC меняем данные
+            }
+        });
+
+
+
+
+
+
+        return binding.getRoot();
     }
 }
